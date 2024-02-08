e0e6079a5dba6ce8397176fa584c32e7c29dd3de
Michael Anello <michael@anelloconsulting.com>
2024-02-08 16:48:00 -0500

    End of class 4.

---
+++ web/modules/custom/drupaleasy_repositories/tests/src/Functional/AddYmlRepoTest.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\Tests\drupaleasy_repositories\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Test description.
+ *
+ * @group drupaleasy_repositories
+ */
+final class AddYmlRepoTest extends BrowserTestBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'claro';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected static $modules = [
+    'drupaleasy_repositories',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp(): void {
+    parent::setUp();
+    // Set up the test here.
+  }
+
+  /**
+   * Test callback.
+   *
+   * @test
+   */
+  public function testSomething(): void {
+    $admin_user = $this->drupalCreateUser([
+      'access administration pages',
+      'administer site configuration',
+    ]);
+
+    $this->drupalLogin($admin_user);
+    $this->drupalGet('admin');
+    $this->assertSession()->elementExists('xpath', '//h1[text() = "Administration"]');
+  }
+
+}
