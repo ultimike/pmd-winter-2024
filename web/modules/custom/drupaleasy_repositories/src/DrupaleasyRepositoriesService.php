7f3e197441d332a863d0bc317c7ddb8e793c28cc
Michael Anello <michael@anelloconsulting.com>
2024-03-12 15:19:32 -0400

    End of class 11.

--- web/modules/custom/drupaleasy_repositories/src/DrupaleasyRepositoriesService.php
+++ web/modules/custom/drupaleasy_repositories/src/DrupaleasyRepositoriesService.php
@@ -6,11 +6,13 @@ namespace Drupal\drupaleasy_repositories;
 
 use Drupal\Component\Plugin\PluginManagerInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Custom functionality for Drupaleasy repositories plugin stuff.
  */
 final class DrupaleasyRepositoriesService {
+  use StringTranslationTrait;
 
   /**
    * The DrupalEasy Repositories plugin manager.
@@ -44,12 +46,14 @@ final class DrupaleasyRepositoriesService {
    *
    * @return string
    *   The help text.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException
    */
   public function getValidatorHelpText(): string {
     // Determine which plugins of our type are enabled (config).
     $repository_plugin_ids = $this->configFactory->get('drupaleasy_repositories.settings')->get('repositories_plugins') ?? [];
 
-    // Instantiate all of the enabled plugins (plugin manager).
+    // Instantiate all the enabled plugins (plugin manager).
     $repository_plugins = [];
     foreach ($repository_plugin_ids as $repository_plugin_id) {
       if (!empty($repository_plugin_id)) {
@@ -66,6 +70,64 @@ final class DrupaleasyRepositoriesService {
 
     // Concatenate results and return.
     return implode(' ', $help);
+  }
+
+  /**
+   * Validates repository URLs.
+   *
+   * @param array<int, array<string, mixed>> $urls
+   *   The repository URLs.
+   * @param int $uid
+   *   The user ID.
+   *
+   * @return string
+   *   An error message, if any.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\PluginException
+   */
+  public function validateRepositoryUrls(array $urls, int $uid): string {
+    $errors = [];
+    $repository_plugins = [];
+
+    // Determine which plugins of our type are enabled (config).
+    $repository_plugin_ids = $this->configFactory->get('drupaleasy_repositories.settings')->get('repositories_plugins') ?? [];
+
+    // Instantiate and loop around each of the enabled plugins (plugin manager).
+    $atLeastOne = FALSE;
+    foreach ($repository_plugin_ids as $repository_plugin_id) {
+      if (!empty($repository_plugin_id)) {
+        $repository_plugins[] = $this->pluginManagerDrupaleasyRepositories->createInstance($repository_plugin_id);
+        $atLeastOne = TRUE;
+      }
+    }
+    if (!$atLeastOne) {
+      return 'There are no enabled repository plugins.';
+    }
+
+    // Loop around each URL to validate.
+    foreach ($urls as $url) {
+      if (is_array($url)) {
+        if ($uri = trim($url['uri'])) {
+          // Loop around the enabled plugins and call their validate() methods.
+          $is_valid_url = FALSE;
+          /** @var \Drupal\drupaleasy_repositories\DrupaleasyRepositories\DrupaleasyRepositoriesInterface $repository_plugin*/
+          foreach ($repository_plugins as $repository_plugin) {
+            if ($repository_plugin->validate($uri)) {
+              $is_valid_url = TRUE;
+              break;
+            }
+          }
+          if (!$is_valid_url) {
+            $errors[] = $this->t('The repository url %uri is not valid.', ['%uri' => $uri]);
+          }
+        }
+      }
+    }
+
+    if ($errors) {
+      return implode(' ', $errors);
+    }
+    return '';
   }
 
 }
--- web/modules/custom/drupaleasy_repositories/src/Form/SettingsForm.php
+++ web/modules/custom/drupaleasy_repositories/src/Form/SettingsForm.php
@@ -4,13 +4,47 @@ declare(strict_types=1);
 
 namespace Drupal\drupaleasy_repositories\Form;
 
+use Drupal\Component\Utility\Unicode;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\TypedConfigManagerInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\drupaleasy_repositories\DrupaleasyRepositories\DrupaleasyRepositoriesPluginManager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Configure DrupalEasy Repositories settings for this site.
  */
 final class SettingsForm extends ConfigFormBase {
+
+  /**
+   * Constructs a new SettingsForm object.
+   *
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The Drupal core configuration factory.
+   * @param \Drupal\Core\Config\TypedConfigManagerInterface $typed_config
+   *   The Drupal core typed configuration manager.
+   * @param \Drupal\drupaleasy_repositories\DrupaleasyRepositories\DrupaleasyRepositoriesPluginManager $repositoriesManager
+   *   The DrupaleasyRepositories plugin manager.
+   */
+  public function __construct(
+    ConfigFactoryInterface $config_factory,
+    TypedConfigManagerInterface $typed_config,
+    protected DrupaleasyRepositoriesPluginManager $repositoriesManager
+  ) {
+    parent::__construct($config_factory, $typed_config);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container): self {
+    return new static(
+      $container->get('config.factory'),
+      $container->get('config.typed'),
+      $container->get('plugin.manager.drupaleasy_repositories'),
+    );
+  }
 
   /**
    * {@inheritdoc}
@@ -28,19 +62,34 @@ final class SettingsForm extends ConfigFormBase {
 
   /**
    * {@inheritdoc}
+   *
+   * @param array<string, mixed> $form
+   *   The form array.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The form state object.
+   *
+   * @return array<string, mixed>
+   *   The form array.
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
     // Use the PHP Null Coalescing Operator in case the config doesn't exist
     // yet.
     $repositories_config = $this->config('drupaleasy_repositories.settings')
       ->get('repositories_plugins') ?? [];
+
+    $repositories = $this->repositoriesManager->getDefinitions();
+    uasort($repositories, static function ($a, $b) {
+      return Unicode::strcasecmp($a['label'], $b['label']);
+    });
+    $repositories_options = [];
+    foreach ($repositories as $id => $repository) {
+      $repositories_options[$id] = $repository['label'];
+    }
+
     $form['repositories_plugins'] = [
       '#type' => 'checkboxes',
       '#title' => $this->t('Repository plugins'),
-      '#options' => [
-        'yml_remote' => $this->t('Yml remote'),
-        'github' => $this->t('GitHub'),
-      ],
+      '#options' => $repositories_options,
       '#default_value' => $repositories_config,
     ];
     return parent::buildForm($form, $form_state);
